0139ec298fb952c7249e047abca4ebac14966ca9
BuriedInTheGround <simone99.as@gmail.com>
2019-11-01 18:12:42 +0100

    Updates

---
+++ precache-manifest.ba945b16dac8951fd931780f827f82e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2897d44b3e7af9329f3bb9c1e6aab58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "221fd275315bd7e3ad16",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "a5d8aed60afec4a649a8",
+    "url": "/static/css/main.2a0a2d10.chunk.css"
+  },
+  {
+    "revision": "221fd275315bd7e3ad16",
+    "url": "/static/js/2.3d3f3796.chunk.js"
+  },
+  {
+    "revision": "a5d8aed60afec4a649a8",
+    "url": "/static/js/main.a7917a49.chunk.js"
+  },
+  {
+    "revision": "c0791672ec2bfe51407b",
+    "url": "/static/js/runtime-main.a8fb7d54.js"
+  }
+]);
